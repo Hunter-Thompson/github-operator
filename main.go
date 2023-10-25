c409622e12ff0ee260875dec1dc5285679ad4491
Aatman <aatman.bos@smallcase.com>
2023-10-25 03:20:39 -0600

    feat(settings): invite user to org (#2)
    ```
    apiVersion: settings.github.com/v1beta1
    kind: InviteUser
    metadata:
      name: inviteuser-sample
    spec:
      organization: "octocat"
      users:
        - username: Hunter-Thompson
          email: aatman@auroville.org.in
    ```

--- main.go
+++ main.go
@@ -103,6 +103,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Team")
 		os.Exit(1)
 	}
+	if err = (&settingscontrollers.InviteUserReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InviteUser")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
